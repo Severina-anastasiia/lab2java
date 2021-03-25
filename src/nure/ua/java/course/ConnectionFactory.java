7c39e3ce6e885d0367d8cbe895034c0d2e67cc60
Anastasiia Severina <71123615+Severina-anastasiia@users.noreply.github.com>
2021-03-25 02:00:39 +0200

    Update ConnectionFactory.java
    make good looking output

--- src/nure/ua/java/course/ConnectionFactory.java
+++ src/nure/ua/java/course/ConnectionFactory.java
@@ -11,7 +11,7 @@ public class ConnectionFactory {
     private static final String dbUser = "root";
     private static final String dbPassword = "root";
 
-    private static String dbUrl = "jdbc:mysql://localhost:3306/lab2java";
+    private static String dbUrl = "jdbc:mysql://" + dbHost + "/" + dbName;
 
     public static Connection getConnection() throws SQLException {
         try {
